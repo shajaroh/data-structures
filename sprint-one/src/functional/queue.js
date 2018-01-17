3cde6225bd1b7926fd9b52b669b53ef6ed016ab0
Shajaroh Chowdhry <shajaroh@gmail.com>
2018-01-17 14:53:06 -0800

    Functional tests passed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var front = 0;
+  var back = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[back] = value;
+    back++;
   };
 
   someInstance.dequeue = function() {
+    var val = storage[front]
+    delete storage[front]
+    front++;
+    return val;
+    
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+   
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
+    
   };
 
   someInstance.pop = function() {
+    count--;
+    return storage[count];
   };
 
   someInstance.size = function() {
+    if(count < 0) {
+      return 0;
+    } 
+    return count;
   };
-
+  
   return someInstance;
 };
