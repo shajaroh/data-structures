98b8965fa27c70e5462a9ef2bcc04cd005615249
shajaroh <shajaroh@gmail.com>
2018-01-19 22:09:10 -0800

    working on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,26 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = null;
+  tree.left = null;
+  tree.right = null;
+
+  return tree;
+
 };
+
+BinarySearchTree.prototype.insert = function() {
+
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
+
+
 
 
 /*
