a0981f958e190f4a04cf429b3fbd4e0338d5f96f
shajaroh <shajaroh@gmail.com>
2018-01-19 20:14:16 -0800

    making progress

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,31 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push({ "value" : node, "edge" : null });
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+
+  this.nodes.forEach(function(obj){
+    if (obj.value === node){
+      return true;
+    }
+  });
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // for (var key in this){
+  //   if (this[key] === node){
+  //     delete this[key];
+  //   }
+  // }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -37,3 +50,9 @@ Graph.prototype.forEachNode = function(cb) {
  */
 
 
+// var test = new Graph();
+//
+// test.addNode(1);
+// //test.contains(1);
+//
+// console.log(test);
